b37718e2b59dc7d85197a623ef4c8eb19b2d5e3e
w1y0n0 <wiyono30@gmail.com>
2024-10-31 16:02:58 +0700

    Latihan RecyclerView: Grid

--- MyRecyclerView/app/src/main/java/com/labsi/myrecyclerview/MainActivity.kt
+++ MyRecyclerView/app/src/main/java/com/labsi/myrecyclerview/MainActivity.kt
@@ -2,11 +2,10 @@ package com.labsi.myrecyclerview
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -22,6 +21,23 @@ class MainActivity : AppCompatActivity() {
         
         list.addAll(getListHeroes())
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_list -> {
+                rvHeroes.layoutManager = LinearLayoutManager(this)
+            }
+            R.id.action_grid -> {
+                rvHeroes.layoutManager = GridLayoutManager(this, 2)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showRecyclerList() {
