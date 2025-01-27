a361c8b4e3db82494cb7c3afc92c508383842f93
w1y0n0 <wiyono30@gmail.com>
2025-01-27 22:13:30 +0700

    Latihan Fragment di Activity

---
+++ MyFlexibleFragment/app/src/main/java/com/labsi/myflexiblefragment/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.labsi.myflexiblefragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        /*
+        Kode di bawah ini digunakan untuk memvalidasi apakah suatu fragment adalah instance dari suatu kelas
+         */
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name : " + HomeFragment::class.java.simpleName)
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
+    }
+}
