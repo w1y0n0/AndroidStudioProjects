f21c58a10180d2dd373a47fbf1a266ac1c653181
w1y0n0 <wiyono30@gmail.com>
2025-02-01 22:52:12 +0700

    Latihan Navigation Drawer dengan NavigationComponent

---
+++ MyNavigationDrawer/app/src/main/java/com/labsi/mynavigationdrawer/SubwayActivity.kt
@@ -0,0 +1,11 @@
+package com.labsi.mynavigationdrawer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
