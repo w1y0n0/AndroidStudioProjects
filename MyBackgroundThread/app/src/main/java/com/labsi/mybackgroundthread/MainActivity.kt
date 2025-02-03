d11f83ceb2e9596a0d6f97fc169a6b4149ec73ab
w1y0n0 <wiyono30@gmail.com>
2025-02-03 12:09:08 +0700

    Latihan Simulasi Proses Asynchronous dengan Background Thread: Coroutine

--- MyBackgroundThread/app/src/main/java/com/labsi/mybackgroundthread/MainActivity.kt
+++ MyBackgroundThread/app/src/main/java/com/labsi/mybackgroundthread/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -16,27 +18,43 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
+//        val executor = Executors.newSingleThreadExecutor()
+//        val handler = Handler(Looper.getMainLooper())
 
         btnStart.setOnClickListener {
-            executor.execute {
-                try {
-                    //simulate process in background thread
-                    for (i in 0..10) {
-                        Thread.sleep(500)
-                        val percentage = i * 10
-                        handler.post {
-                            //update ui in main thread
-                            if (percentage == 100) {
-                                tvStatus.setText(R.string.task_completed)
-                            } else {
-                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                            }
+//            executor.execute {
+//                try {
+//                    //simulate process in background thread
+//                    for (i in 0..10) {
+//                        Thread.sleep(500)
+//                        val percentage = i * 10
+//                        handler.post {
+//                            //update ui in main thread
+//                            if (percentage == 100) {
+//                                tvStatus.setText(R.string.task_completed)
+//                            } else {
+//                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+//                            }
+//                        }
+//                    }
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//            }
+
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        //update ui in main thread
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
                         }
                     }
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
                 }
             }
         }
