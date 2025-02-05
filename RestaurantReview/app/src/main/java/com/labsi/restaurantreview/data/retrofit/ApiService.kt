6b85e0f65e5160f08e47354880806afd7fae3e58
w1y0n0 <wiyono30@gmail.com>
2025-02-05 09:50:38 +0700

    Latihan Networking dengan Retrofit

---
+++ RestaurantReview/app/src/main/java/com/labsi/restaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,12 @@
+package com.labsi.restaurantreview.data.retrofit
+
+import com.labsi.restaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
